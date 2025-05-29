282ff91d7d6a6304a5c0c3de874b83c9248aa3a7
I SHOW AKIRU <a69881018@gmail.com>
2025-05-29 12:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,25 @@ with open('itemData.json', 'r') as f:
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['GET'])
-def search():
+@app.route('/api/search', methods=['GET'])
+def search_items():
     query = request.args.get('q', '').lower()
+    
     if not query:
         return jsonify([])
     
     results = []
     for item in items:
-        if (query in str(item['itemID']) or 
-            query in item['icon'].lower() or 
-            query in item['description'].lower() or 
-            (item['description2'] and query in item['description2'].lower())):
-            results.append(item)
+        # Search in itemID, icon, description, description2
+        if (query in str(item['itemID']) or \
+           (query in item['icon'].lower()) or \
+           (query in item.get('description', '').lower()) or \
+           (query in item.get('description2', '').lower()):
+            
+            # Add image URL to the item data
+            item_with_image = item.copy()
+            item_with_image['image_url'] = f"https://raw.githubusercontent.com/I-SHOW-AKIRU200/AKIRU-ICONS/main/ICONS/{item['itemID']}.png"
+            results.append(item_with_image)
     
     return jsonify(results)
 
